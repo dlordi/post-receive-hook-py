5ccc3f7536293519ba83e16029913ba51a5a8846
dlordi <domenico.lordi@gmail.com>
2026-04-03 11:07:31 +0200

    Add TODOs on what to implement to actually perform deployment

--- .deploy/deployment.py
+++ .deploy/deployment.py
@@ -2,6 +2,7 @@
 
 import datetime
 import os
+# import shutil
 import sys
 
 
@@ -11,14 +12,26 @@ def main():
         if not line:
             continue
 
-        status, path = line.split('\t', 1)
-        if os.path.exists(path):
+        repo_status, repo_path = line.split('\t', 1)
+        deploy_path = get_deploy_path_from_repo_path(repo_path)
+        deploy_path_exists = os.path.exists(deploy_path)
+        if deploy_path_exists:
             timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-            print(f'backup existing {path} to {path}.{timestamp}...')
-        if status in ('A', 'M'):
-            print(f'installing {path}...')
-        elif status == 'D':
-            print(f'deleting {path}...')
+            print(f'TODO: backup existing {deploy_path} to {deploy_path}.{timestamp}...')
+            # shutil.copy2(deploy_path, f'{deploy_path}.{timestamp}')
+
+        if repo_status in ('A', 'M'):
+            print(f'TODO: installing {repo_path} to {deploy_path}...')
+            # shutil.copy2(repo_path, deploy_path)
+        elif repo_status == 'D':
+            print(f'TODO: uninstalling {repo_path} from {deploy_path}...')
+            # if deploy_path_exists:
+            #     os.remove(deploy_path_exists)
+
+
+def get_deploy_path_from_repo_path(repopath):
+    print('TODO: convert {repopath} to deployment path...')
+    return repopath
 
 
 if __name__ == '__main__':
