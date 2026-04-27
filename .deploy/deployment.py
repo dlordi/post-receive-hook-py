488bba9f26c0591dbb82c3541bda6b580660850f
Domenico Lordi <lordi.tecnoeng@gmail.com>
2026-04-27 16:00:46 +0200

    chore: Remove utility function in favor of python standard library

--- .deploy/deployment.py
+++ .deploy/deployment.py
@@ -9,17 +9,12 @@ WEB_ASSETS_REGEX = re.compile(r"""html/(.*\.(css|js))$""")
 def get_deploy_info_from_repo_path(repo_path: str) -> str | None:
     deploy_path = repo_path  # TODO: convert to deploy path; set to None (or leave equal to repo_path) to ignore
 
+    # tips:
+    # - use os.path.normpath(repo_path) to generate a path with the OS-specific path separator
+
     if BIN_PY_REGEX.match(deploy_path):
         pass
     elif WEB_ASSETS_REGEX.match(deploy_path):
         pass
 
     return deploy_path
-
-
-def repo_path_to_os_path(repo_path: str) -> str:
-    """Utility to convert repo path to OS specific path"""
-    retval = repo_path
-    if os.path.sep != '/':
-        retval = retval.replace('/', os.path.sep)
-    return retval
