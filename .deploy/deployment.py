bb18744799ea86168685c4653b22fdaf2f6286bd
Domenico Lordi <lordi.tecnoeng@gmail.com>
2026-04-27 15:43:33 +0200

    fix: Add import required by sample deployment script

--- .deploy/deployment.py
+++ .deploy/deployment.py
@@ -1,16 +1,19 @@
+import os
 import re
 
 # sample regex used to define deployment rules (change them according to your needs!)
-BIN_PY_REGEX = re.compile(r"""bin/.*\.py$""")
-ASSETS_REGEX = re.compile(r"""html/.*\.(css|js)$""")
+BIN_PY_REGEX = re.compile(r"""bin/(.*\.py)$""")
+WEB_ASSETS_REGEX = re.compile(r"""html/(.*\.(css|js))$""")
 
 
 def get_deploy_info_from_repo_path(repo_path: str) -> str | None:
     deploy_path = repo_path  # TODO: convert to deploy path; set to None (or leave equal to repo_path) to ignore
+
     if BIN_PY_REGEX.match(deploy_path):
         pass
-    elif ASSETS_REGEX.match(deploy_path):
+    elif WEB_ASSETS_REGEX.match(deploy_path):
         pass
+
     return deploy_path
 
 
